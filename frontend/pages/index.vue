57145057ced1eb8be5683e9be33d486f2dcbb0d2
RomelDonovan <romeldonovan@live.ca>
2024-01-26 09:40:20 -0500

    Create pages dir and add file upload

---
+++ frontend/pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>Assist Me</h1>
+  <main>
+    <div>
+      <label>File
+        <input type="file" id="file" />
+      </label>
+      <button>Submit</button>
+    </div>
+  </main>
+</template>
