5a4f9e2ada7efba2e9183cfdd6450aed7e2b4ec9
RomelDonovan <romeldonovan@live.ca>
2024-01-26 11:00:27 -0500

    Create a /profile page

---
+++ frontend/pages/profile.vue
@@ -0,0 +1,22 @@
+<template>
+  <section class="section">
+    <div class="container">
+      <h2 class="title">My Profile</h2>
+      <div class="content">
+        <main>
+          <div>
+            <label>File
+              <input type="file" id="file" />
+            </label>
+            <button>Submit</button>
+          </div>
+        </main>
+        <h2>Comments</h2>
+        <form>
+          <textarea name="text" id="" cols="30" rows="10"></textarea>
+        </form>
+        <button>Submit</button>
+      </div>
+    </div>
+  </section>
+</template>
