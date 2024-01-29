37b3aeded31e989c1b06bd8913c5e72639ce75a8
RomelDonovan <romeldonovan@live.ca>
2024-01-29 15:36:36 -0500

    Add to Mutation an create, delete, and edit to User and patient in schema

--- backend/src/schema.ts
+++ backend/src/schema.ts
@@ -17,15 +17,60 @@ export const typeDefs = `#graphql
     role: String!
     comments: [Comment!]
   }
+  
+  type Patient {
+    id: ID!
+    name: String!
+  }
 
   type Query {
     comments: [Comment]
     comment(id: ID!): Comment
     users: [User]
     user(id: ID!): User
+    patients: [Patient]
+    patient(id: ID!): Patient
   }
 
   type Mutation {
+    addComment(comment: AddCommentInput!): Comment
     deleteComment(id: ID!): [Comment]
+    editComment(id: ID!, edit: EditCommentInput!): Comment
+    createUser(user: CreateUserInput!): User
+    deleteUser(id: ID!): [User]
+    editUser(id: ID!, edit: EditUserInput!): User
+    createPatient(patient: CreatePatientInput!): Patient
+    deletePatient(id: ID!): [Patient]
+    editPatient(id: ID!, edit: EditPatientInput!): Patient
+  }
+  
+  input AddCommentInput {
+    comment: String!
+  }
+
+  input EditCommentInput{
+    comment: String
+  }
+
+  input CreateUserInput {
+    name: String!
+    email: String!
+    password: String!
+    role: String!
+  }
+
+  input EditUserInput {
+    name: String
+    email: String
+    password: String
+    role: String
+  }
+
+  input CreatePatientInput {
+    name: String!
+  }
+
+  input EditPatientInput {
+    name: String
   }
 `;
