9088b551c309df8aa9361f0a04ca8b9c8a5a7751
RomelDonovan <romeldonovan@live.ca>
2024-01-29 13:58:19 -0500

    Create a schema to execute the data

---
+++ backend/src/schema.ts
@@ -0,0 +1,27 @@
+// A schema is a collection of type definitions (hence "typeDefs")
+// that together define the "shape" of queries that are executed against
+// your data.
+export const typeDefs = `#graphql
+
+  type Comment {
+    id: ID!
+    comment: String!
+    user: User!
+  }
+
+  type User {
+    id: ID!
+    name: String!
+    email: String!
+    password: String!
+    role: String!
+    comments: [Comment!]
+  }
+
+  type Query {
+    comments: [Comment]
+    comment(id: ID!): Comment
+    users: [User]
+    user(id: ID!): User
+  }
+`;
