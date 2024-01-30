1e398fd4b808e4bbc651ff95b9c77a3f4a82573c
RomelDonovan <romeldonovan@live.ca>
2024-01-30 00:51:18 -0500

    Update resolver to store data into firebase database

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,107 +1,160 @@
-import { initializeApp, applicationDefault } from 'firebase-admin/app';
-const app = initializeApp({
-  credential: applicationDefault(),
-  databaseURL: 'https://assist-me-87dc1-default-rtdb.firebaseio.com/'
-});
-
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 import { typeDefs } from './schema.js';
-import db from './db.js';
+// import db from './db.js';
+
+import admin from 'firebase-admin';
+
+const serviceAccount: admin.ServiceAccount = {
+  projectId: "sentry-health-79f48",
+  privateKey: "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDJjDK2y6bJB2za\nI49ZASwuPoObIWwW9aysQubUGzVN3bL2xQSQlPjpTfTWrv4QsBJ0SFFPcMNDnjZR\nMQ/+AONi+n5DGycsHv1nx7nxp0krI3qAlojgZLgBjyhPS7CuiXcfulMD0dVncMJy\nToWMIgIcyvjgqCWihA12rEHcM3GjxLcMsQm8TrTyjySc3cwvl8Bb/82SaKZR7nPg\nD79U/7wfgMgmP6hoA78DYBbRJxdz+8+UAHbrLRq/UvO8XgQ6PIicAV7ADp0stXqw\nKcRc4/KkI0dBiqT5qaOT7Hf+Yfpy7r57JqcGaUTu4Xx7S8llxOt8rltqBidFJONr\naldOzvJRAgMBAAECggEAF8jZu0SxPAwKRnvn0MMYqaRq94WsMdFwPSvv/ZvwYG4H\nTqPVdBKAhVfTyvn/tYxxwRM/tTZDdNlY45/8mScVrnqNJ/IuX0CQ+wxQ3zm0osif\nDkm1gXWhWV3BjgoKoa5LRo1AerTqkDh4WfD2RaFFyowRbsGpH1KiHB0b8JaJlJ/O\ntZWWQ7/tXtH7mqFbrOjPAslMTJgSm9PiUnlwHcWIMN54+Un3neDK0wrl/4Y/gwOz\nQIeBfvPdCpihn6G41DFbXIfBPiJzY8Q/74tWD6SeL38MYqObzQ/7P6wOU8gWhoZi\nWAUuPhxqum5fqKzGjr7QFmsr25yJ9s/313rqsVKyoQKBgQDjQa+aHvy4uPL6CtiE\nEJ0TtgQDP7TW7wfrOFJRsSq1haUogKy9jkZdqpa0Mohkc5ePUBF3WS4f0kwTHN6N\nUD9n6OQ6dCZ0qkmrUosxN6+aKy2nFAt8Tm/2cULp3o0z2OSAx+rKv0pBYNDPd57E\nuoK1xHlAyUp0UGl5SZvNOfTdtQKBgQDjChXlrFdxYFvv5VYJfF+AX4kHGW0RQoMH\nSUdfO/lfGUXyq8QCOYT0Hs24Vg4l0Z0K9z9doIPpkOK5bFiW02h0YbczECMoxF6U\nYyNjLunf+4iYpCEje6JDjlQW5sCCEC3UGImSa7VlLHK3gXGYUEC50P3AK88LOUCn\n6oDyJEwDrQKBgDhy/YKEBfSAhGJVxVhwpAPcZ3zDG1F0SHxGHjSKlHhIfTDlXYMs\nTxA/KnYSQ0zoZZJEqkICFfHzk8//kMx7TXSrJd3bMh4ymmAC/e44zda2uP1SflPf\nS/JkSeG71Uk2q/PNNOYtw+0fdl7CcN6fEY7DAfd0yJjBzKFDBn9g0REtAoGBAMny\nkKOodmc8pMpwHeV2IaSYVdRgw2kU1mPGxJtrR1dVrag7LLAl5aMXOzl6Ij91gdDd\nOBnZyzwYSfNQ8kPGbCUJPeFHu15pvQ8UV8fDVPiMTmYnp+pd8GlS0m1Ed3u5VGpS\nIRyCfekubiPYGei3ibs+sFcdIJ8FIAO8TVIVAyBJAoGBAJ0AZZP7e5DMFfWSJUhk\nYSHrMRD8ANB5WftISkEYEG8R9XLNUEcuf2ScEzwdNkUYne8/glCdxTK8TcGpKzgI\nvC2MMjdr+Sc0J83QyYmyC9RZURF8dizuhYopwmwjVTAl8GH/KrnV3iZ767VYByrV\n9cZaLKTyuAU6iRCJCH1OixRE\n-----END PRIVATE KEY-----\n",
+  clientEmail: "firebase-adminsdk-gddfy@sentry-health-79f48.iam.gserviceaccount.com"
+}
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
 
 // Resolvers define how to fetch the types defined in your schema.
 const resolvers = {
   Query: {
-    comments() {
-      return db.comments;
+    async comments() {
+      const commentsDoc = await admin
+        .firestore()
+        .collection('comments')
+        .get();
+      const comments = commentsDoc.docs.map((comment) => {
+        return {
+          id: comment.id,
+          ...comment.data(),
+        };
+      });
+      return comments;
     },
-    comment(_, args) {
-      return db.comments.find((comment) => comment.id === args.id)
+    async comment(_, args) {
+      const commentDoc = await admin
+        .firestore()
+        .doc(`comments/${args.id}`)
+        .get();
+      const comment = commentDoc.data();
+      return comment;
     },
-    users() {
-      return db.users;
+    async users() {
+      const usersDoc = await admin
+        .firestore()
+        .collection('users')
+        .get();
+      const users = usersDoc.docs.map((user) => {
+        return {
+          id: user.id,
+          ...user.data(),
+        };
+      });
+      return users;
     },
-    user(_, args) {
-      return db.users.find((user) => user.id === args.id)
+    async user(_, args) {
+      const userDoc = await admin
+        .firestore()
+        .doc(`users/${args.id}`)
+        .get();
+      const user = userDoc.data();
+      return user;
     }
   },
   User: {
-    comments(parent) {
-      return db.comments.filter((comment) => comment.user_id === parent.id)
+    async comments(parent) {
+      const comments = await admin
+        .firestore()
+        .collection('comments')
+        .where('user_id', '==', parent.id)
+        .get();
+      return comments.docs.map((comment) => comment.data());
     }
   },
   Mutation: {
-    deleteComment(_, args) {
-      db.comments = db.comments.filter((comment) => comment.id !== args.id)
-      return db.comments;
+    // Comments
+    async deleteComment(_, args) {
+      await admin
+        .firestore()
+        .doc(`comments/${args.id}`)
+        .delete();
+      return args.id;
     },
-    addComment(_, args) {
-      let comment = {
-        ...args.comment,
-        id: String(db.comments.length + 1),
-      }
-      db.comments.push(comment)
+    async addComment(_, args) {
+      await admin
+        .firestore()
+        .collection('comments')
+        .add(args.comment);
+      return args.comment;
+    },
+    async editComment(_, args) {
+      await admin
+        .firestore()
+        .doc(`comments/${args.id}`)
+        .update(args.edit);
+      const commentDoc = await admin
+        .firestore()
+        .doc(`comments/${args.id}`)
+        .get();
+      const comment = commentDoc.data();
       return comment;
     },
-    editComment(_, args) {
-      db.comments = db.comments.map((comment) => {
-        if (comment.id === args.id) {
-          return {
-            ...comment,
-            ...args.edit
-          }
-        }
-        return comment;
-      })
-      return db.comments.find((comment) => comment.id === args.id)
+
+    // Users
+    async deleteUser(_, args) {
+      await admin
+        .firestore()
+        .doc(`users/${args.id}`)
+        .delete();
+      return args.id;
     },
-    deleteUser(_, args) {
-      db.users = db.users.filter((user) => user.id !== args.id)
-      return db.users;
+    async createUser(_, args) {
+      await admin
+        .firestore()
+        .collection('users')
+        .add(args.user);
+      return args.user;
     },
-    createUser(_, args) {
-      let user = {
-        ...args.user,
-        id: String(db.users.length + 1),
-      }
-      db.users.push(user)
+    async editUser(_, args) {
+      await admin
+        .firestore()
+        .doc(`users/${args.id}`)
+        .update(args.edit);
+      const userDoc = await admin
+        .firestore()
+        .doc(`users/${args.id}`)
+        .get();
+      const user = userDoc.data();
       return user;
     },
-    editUser(_, args) {
-      db.users = db.users.map((user) => {
-        if (user.id === args.id) {
-          return {
-            ...user,
-            ...args.edit
-          }
-        }
-        return user;
-      })
-      return db.users.find((user) => user.id === args.id)
+
+    // Patients
+    async deletePatient(_, args) {
+      await admin
+        .firestore()
+        .doc(`patients/${args.id}`)
+        .delete();
+      return args.id;
     },
-    deletePatient(_, args) {
-      db.patients = db.patients.filter((patient) => patient.id !== args.id)
-      return db.patients;
+    async createPatient(_, args) {
+      await admin
+        .firestore()
+        .collection('patients')
+        .add(args.patient);
+      return args.patient;
     },
-    createPatient(_, args) {
-      let patient = {
-        ...args.patient,
-        id: String(db.patients.length + 1),
-      }
-      db.patients.push(patient)
+    async editPatient(_, args) {
+      await admin
+        .firestore()
+        .doc(`patients/${args.id}`)
+        .update(args.edit);
+      const patientDoc = await admin
+        .firestore()
+        .doc(`patients/${args.id}`)
+        .get();
+      const patient = patientDoc.data();
       return patient;
-    },
-    editPatient(_, args) {
-      db.patients = db.patients.map((patient) => {
-        if (patient.id === args.id) {
-          return {
-            ...patient,
-            ...args.edit
-          }
-        }
-        return patient;
-      })
-      return db.patients.find((patient) => patient.id === args.id)
     }
   }
 };
