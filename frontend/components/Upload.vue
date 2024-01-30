183b37ae6d80232fff5aff4fcf811318efa3fdd7
RomelDonovan <romeldonovan@live.ca>
2024-01-30 08:38:53 -0500

    Add file upload

---
+++ frontend/components/Upload.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <input type="file" @change="onFileChange" />
+    <button @click="uploadFile">Upload</button>
+  </div>
+</template>
+
+<script>
+
+</script>
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,11 +1,8 @@
 <template>
-  <h1>Assist Me</h1>
-  <main>
-    <div>
-      <label>File
-        <input type="file" id="file" />
-      </label>
-      <button>Submit</button>
-    </div>
-  </main>
+  <div>
+    <Patients />
+  </div>
+  <div>
+    <Upload />
+  </div>
 </template>
