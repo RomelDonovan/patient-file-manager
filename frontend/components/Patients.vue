02e12a49b51d1c8ff2b3d04630b048eac7600aa8
RomelDonovan <romeldonovan@live.ca>
2024-01-30 23:54:29 -0500

    Update Patients component to sort patients names

--- frontend/components/Patients.vue
+++ frontend/components/Patients.vue
@@ -1,4 +1,59 @@
 <template>
-  <h2>Patient List</h2>
-  <PatientsList />
+  <div>
+    <ul class="list">
+      <li v-for="patient in patients" :key="patient.id">{{ patient.name }}</li>
+    </ul>
+  </div>
 </template>
+
+<style>
+.list {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+</style>
+
+<script>
+export default {
+  data() {
+    return {
+      patients: [],
+    };
+  },
+  mounted() {
+    this.fetchData();
+  },
+  methods: {
+    fetchData() {
+      const firestoreUrl = 'https://firestore.googleapis.com/v1/projects/sentry-health-79f48/databases/(default)/documents/patients';
+
+      fetch(firestoreUrl)
+        .then((response) => response.json())
+        .then((data) => {
+          if (data.documents && data.documents.length > 0) {
+            this.patients = data.documents.map((document) => ({
+              id: document.name.split('/').pop(),
+              ...document.fields,
+            }));
+
+            this.patients.forEach((patient) => {
+              Object.keys(patient).forEach((key) => {
+                if (patient[key] && patient[key].hasOwnProperty('stringValue')) {
+                  patient[key] = patient[key].stringValue;
+                }
+              });
+            });
+            this.patients.sort((a, b) => a.name.localeCompare(b.name));
+          } else {
+            console.error('No documents found in the response:', data);
+          }
+        })
+        .catch((error) => {
+          console.error('Error fetching data:', error);
+        });
+    },
+  },
+};
+</script>
